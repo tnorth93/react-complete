37b973322291c8a25342f0f04b4f577ed2a20257
Tom <tnorth93@gmail.com>
2019-12-09 21:04:41 -0800

    added nameSwitchHandler to button onClick

--- src/App.js
+++ src/App.js
@@ -11,11 +11,22 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+   // Don't do this!!!!!!!! this.state.persons[0].name = 'Thomas';
+   this.setState({
+    persons: [
+      { name: 'Thomas', age: 26},
+      { name: 'Kelly', age: 27},
+      { name: 'Nero Beans', age: 4}
+    ]
+   })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button> Switch Name </button>
+        <button onClick={this.switchNameHandler}> Switch Name </button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>

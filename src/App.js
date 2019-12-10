b4cb5b803fb809b78c6e06f5ff7fd861f97f1ae4
Tom <tnorth93@gmail.com>
2019-12-09 20:50:23 -0800

    Added state to App.js to dynamically change the person

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Tom', age: 26},
+      { name: 'Kelly', age: 27},
+      { name: 'Nero Beans', age: 4}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person/>
+        <button> Switch Name </button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }

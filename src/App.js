65134346ca9976f5cc14fa64e24f7d25555466ba
Tom <tnorth93@gmail.com>
2019-12-10 22:08:20 -0800

    refactored state and switchNameHandler

--- src/App.js
+++ src/App.js
@@ -3,37 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 const app = props => {
-  useState({
+  const [ personsState, setPersonsState ] = useState({
     persons: [
-      { name: 'Tom', age: 26},
-      { name: 'Kelly', age: 27},
-      { name: 'Nero Beans', age: 4}
-    ]
+      { name: 'Tom', age: 26 },
+      { name: 'Kelly', age: 27 },
+      { name: 'Nero Beans', age: 4 }
+    ],
+    otherState: 'some other value'
   });
+
+  const switchNameHandler = () => {
+    // Don't do this!!!!!!!! personsState.persons[0].name = 'Thomas';
+    this.setState({
+     persons: [
+       { name: 'Thomas', age: 26},
+       { name: 'Kelly', age: 27},
+       { name: 'Nero Beans', age: 4}
+     ]
+    })
+   }
 
   return (
     <div className="App">
       <h1>Hi, I'm a React App</h1>
-      <button onClick={this.switchNameHandler}> Switch Name </button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <button onClick={switchNameHandler}> Switch Name </button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
     </div>
   );
 }
 
 export default app;
-
-// BELOW IS BEING SAVED FOR USE W/O REACT HOOKS
-
-
-switchNameHandler = () => {
-  // Don't do this!!!!!!!! this.state.persons[0].name = 'Thomas';
-  this.setState({
-   persons: [
-     { name: 'Thomas', age: 26},
-     { name: 'Kelly', age: 27},
-     { name: 'Nero Beans', age: 4}
-   ]
-  })
- }

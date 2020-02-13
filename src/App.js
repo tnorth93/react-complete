259087b9458841ef389658befad20b28af353c9d
Tom <tnorth93@gmail.com>
2020-02-12 21:11:12 -0800

    fixed bug in referencing the original state rather than a copy

--- src/App.js
+++ src/App.js
@@ -34,8 +34,9 @@ class App extends Component {
    }
 
    deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
-    persons.splice(personIndex, 1)
+    const persons = this.state.persons.slice();
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
    }
 
   togglePersonHandler = () => {

31890ddd06c4cbd99affb66ef2633e73c0b12b75
Tom North <tnorth93@gmail.com>
2020-06-03 11:29:16 -0700

    converted person.js to use an array rather than a wrapping div to avoid the error for adj jsx elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,13 +4,20 @@ import classes from './Person.css';
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...')
-    return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </div>  
-    )
+    return [
+      // <div className={classes.Person}>
+        <p key='i1' onClick={this.props.click}>
+          I'm {this.props.name} and I'm {this.props.age} years old
+        </p>,
+        <p key='i2'>{this.props.children}</p>,
+        <input
+          key='i3'
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      // </div>  
+  ];
   }
 }
 

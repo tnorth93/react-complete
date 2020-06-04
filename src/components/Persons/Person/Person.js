eae44b98117c5f12732ac69001daafcc3a5809c4
Tom North <tnorth93@gmail.com>
2020-06-04 16:40:35 -0700

    added focus to person.js

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,6 +6,15 @@ import withClass from '../../../hoc/withClass';
 import Auxiliary from '../../../hoc/Auxiliary';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
   render() {
     console.log('[Person.js] rendering...')
     return (
@@ -16,6 +25,8 @@ class Person extends Component {
         <p key='i2'>{this.props.children}</p>
         <input
           key='i3'
+          // ref={(inputEl) => {this.inputElement = inputEl}}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
